55e36f0f4c880f93b473a06ee3f36bf3682bdbce
Cecilia Pastore <ceciliapastore89@gmail.com>
2023-02-20 22:06:48 +0000

    Week04 Task

--- collatz.py
+++ collatz.py
@@ -4,10 +4,12 @@
 # - if the user enter a negative number he ask him to enter again a positive number
 # - take the positive number and devide it for 2 if it is a even number 
 #   or moltiply per 3 + 1 if it is an odd number
-# - print the new number
+# - add the new numbers to a list
 # - repet the while loop until the number is one
-# - print end at the end  
+# - print the numbers in the list 
 
+# create list numbers
+numbers = []
 # ask to the user to input a positive number 
 number = int(input("Please Enter a positive number: "))
 
@@ -19,15 +21,17 @@ while number <=0:
     
 # second while loop to be excecuted until the result number is 1    
 while number != 1:
-# if the number is an even number devide it for 2 and print the new number
+# if the number is an even number devide it for 2 and and append it to the list
     if (number%2) == 0:
-        number = int(number / 2)
-        print(number)
+        number = int((number / 2))
+        numbers.append(number)
 # if the number is an odd number moltiply it for 3 and add 1
-# print the new number 
+# add the new number to the list numbers
     else:
-        number = int(number * 3 + 1)
-        print(number)
-# when the number become 1 end the while loop and print "End"
-print("End")
-    
+        number = int((number * 3 + 1))
+        numbers.append(number)
+# when the number become 1 exit the loop and enter the for loop
+# the for loop will print all the number in the numebrs list with a vertical 
+# orientation 
+for n in numbers:
+    print(n, end= " ")
