e59ab38ab0529936aa08a273f34f498e14eefa8c
Cecilia Pastore <ceciliapastore89@gmail.com>
2023-03-05 16:51:15 +0000

    Week05Task

---
+++ weekday.py
@@ -0,0 +1,16 @@
+# this programs outputw whethewer ot not today is a weekkaday.
+# if today is a weekday it print: Yes, unfortunately today is a weekday
+# otherwise, it print: It is the weekend, yay!
+# Author: Cecilia Pastore 
+
+# import datatime module 
+import datetime
+
+# return what number of the week today considerin 0 as monday and 6 as sunday using the data funciotn
+weekno = datetime.datetime.today().weekday()
+
+# if stantement to print the correct ouput based on the nuber day of the week
+if weekno < 5:
+    print ("Yes, unfortunately today is a weekday")
+else:  # 5 Sat, 6 Sun
+    print ("It is the weekend, yay!")
